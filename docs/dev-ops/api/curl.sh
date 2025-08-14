d556190bd7d09dc82a62c6a0dacb7d20f8138cea
linnuo <3092544409@qq.com>
2025-08-14 16:10:24 +0800

    初始化知识库工程

--- docs/dev-ops/api/curl.sh
+++ docs/dev-ops/api/curl.sh
@@ -1,7 +1,7 @@
-curl http://117.72.115.188:11434/api/generate \
+curl http://117.72.183.125:11434/api/generate \
   -H "Content-Type: application/json" \
   -d '{
         "model": "deepseek-r1:1.5b",
-        "prompt": "1+1",
+        "prompt": "今天合肥天气怎么样",
         "stream": false
       }'
